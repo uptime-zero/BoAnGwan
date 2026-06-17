e4ec033677664fc14b77fc029735880e78215c8e
uptime-zero <dereckjun2173@gmail.com>
2026-06-18 05:23:43 +0900

    Initial commit

---
+++ src/main/java/com/boangwan/BoAnGwanApplication.java
@@ -0,0 +1,13 @@
+package com.boangwan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoAnGwanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoAnGwanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boangwan/BoAnGwanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boangwan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoAnGwanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
